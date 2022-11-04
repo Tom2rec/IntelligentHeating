8b20d5541c8d0a787f2711fc100ca9cd0778936d
jwozniak <jwozniak@core-logic.com>
2022-11-04 17:54:40 +0100

    chagnes

--- Core/src/main/java/com/heating/system/schedule/repository/RoomRepository.java
+++
@@ -1,8 +0,0 @@
-package com.heating.system.schedule.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import java.util.UUID;
-
-public interface RoomRepository extends JpaRepository<Room, UUID> {
-}
