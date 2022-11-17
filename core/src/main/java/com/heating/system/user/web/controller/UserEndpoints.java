87d4b94ca84982b7643bae80ac40726b48c5fb75
Tomasz Turek <t.turek2000@gmail.com>
2022-11-17 23:00:28 +0100

    Update core name for codeql

--- core/src/main/java/com/heating/system/user/web/controller/UserEndpoints.java
+++ core/src/main/java/com/heating/system/user/web/controller/UserEndpoints.java
@@ -2,11 +2,13 @@ package com.heating.system.user.web.controller;
 
 import com.heating.system.user.model.request.LoginRequest;
 import com.heating.system.user.model.request.UserCreateRequest;
-import com.heating.system.user.model.request.UserUpdateRequest;
-import com.heating.system.user.model.response.UpdateUserResponse;
 import com.heating.system.user.model.response.UserInfoResponse;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.util.List;
 import java.util.UUID;
